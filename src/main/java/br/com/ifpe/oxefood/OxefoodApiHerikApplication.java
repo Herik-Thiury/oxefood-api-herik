6022a56ffe4f1b665bbc6b36ab5e3fd645812515
Hérik Thiury <herikthiury2017@gmail.com>
2026-02-11 20:50:42 -0300

    Ambientação-backend

---
+++ src/main/java/br/com/ifpe/oxefood/OxefoodApiHerikApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodApiHerikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodApiHerikApplication.class, args);
+	}
+
+}
