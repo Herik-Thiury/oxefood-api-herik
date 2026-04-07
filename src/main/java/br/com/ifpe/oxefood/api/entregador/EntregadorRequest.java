0898240302dec8a76861d1d4239e360e34398286
Herik-Thiury <herikthiury2017@gmail.com>
2026-04-07 18:06:47 -0300

    validações de entrada

--- src/main/java/br/com/ifpe/oxefood/api/entregador/EntregadorRequest.java
+++ src/main/java/br/com/ifpe/oxefood/api/entregador/EntregadorRequest.java
@@ -1,8 +1,12 @@
 package br.com.ifpe.oxefood.api.entregador;
 
 import java.time.LocalDate;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.br.CPF;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import br.com.ifpe.oxefood.modelo.entregador.Entregador;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,24 +18,50 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class EntregadorRequest {
 
+   @NotBlank(message = "O Nome é de preenchimento obrigatório")
+   @Length(max = 100, message = "O Nome deverá ter no máximo {max} caracteres")
    private String nome;
+
+   @NotBlank(message = "O CPF é de preenchimento obrigatório")
+   @CPF(message = "O CPF informado é inválido")
    private String cpf;
+
+   @NotBlank(message = "O RG é de preenchimento obrigatório")
    private String rg;
 
+   @NotNull(message = "A Data de Nascimento é de preenchimento obrigatório")
    @JsonFormat(pattern = "dd/MM/yyyy")
    private LocalDate dataNascimento;
 
+   @NotBlank(message = "O Fone Celular é de preenchimento obrigatório")
    private String foneCelular;
+
    private String foneFixo;
+
    private Integer qtdEntregasRealizadas;
+   
+   @NotNull(message = "O Valor do Frete é obrigatório")
    private Double valorFrete;
+
+   @NotBlank(message = "A Rua é obrigatória")
    private String enderecoRua;
+
    private String enderecoNumero;
+
+   @NotBlank(message = "O Bairro é obrigatório")
    private String enderecoBairro;
+
+   @NotBlank(message = "A Cidade é obrigatória")
    private String enderecoCidade;
+
+   @NotBlank(message = "O CEP é obrigatório")
    private String enderecoCep;
+
+   @NotBlank(message = "A UF é obrigatória")
    private String enderecoUf;
+
    private String enderecoComplemento;
+   
    private Boolean ativo;
 
    public Entregador build() {
--- src/main/java/br/com/ifpe/oxefood/api/produto/ProdutoRequest.java
+++ src/main/java/br/com/ifpe/oxefood/api/produto/ProdutoRequest.java
@@ -1,6 +1,9 @@
 package br.com.ifpe.oxefood.api.produto;
 
+import org.hibernate.validator.constraints.Length;
 import br.com.ifpe.oxefood.modelo.produto.Produto;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,11 +15,23 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ProdutoRequest {
 
+   @NotBlank(message = "O Título é de preenchimento obrigatório")
+   @Length(max = 100, message = "O Título deverá ter no máximo {max} caracteres")
    private String titulo;
+
+   @NotBlank(message = "O Código é de preenchimento obrigatório")
    private String codigo;
+
+   @NotBlank(message = "A Descrição é de preenchimento obrigatório")
    private String descricao;
+
+   @NotNull(message = "O Valor Unitário é de preenchimento obrigatório")
    private Double valorUnitario;
+
+   @NotNull(message = "O Tempo de Entrega Mínimo é de preenchimento obrigatório")
    private Integer tempoEntregaMinimo;
+
+   @NotNull(message = "O Tempo de Entrega Máximo é de preenchimento obrigatório")
    private Integer tempoEntregaMaximo;
 
    public Produto build() {
